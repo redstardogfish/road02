ac08796a18123826f3b5e73b06201475c5ada3a8
Tony <ptonyweir@gmail.com>
2011-03-10 08:12:30 +1100

    trying for the big range of photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,12 +26,14 @@ module ApplicationHelper
   end
   
   def generate_image_tag
-    # image_number = (1..83).to_a.sort_by{rand}.first.to_i
-    # image_number = (image_number+1000).to_s
-    # name_fragment = image_number[1..-1]
+    # this one is for the longer set of photos - didn't seem to work on heroku.
+    image_number = (1..83).to_a.sort_by{rand}.first.to_i
+    image_number = (image_number+1000).to_s
+    name_fragment = image_number[1..-1]
 
-    image_number = (1..24).to_a.sort_by{rand}.first.to_s
-    name_fragment = image_number
+    # this one for the 24 placeholder pngs
+    # image_number = (1..24).to_a.sort_by{rand}.first.to_s
+    # name_fragment = image_number
 
     image_name = "placeholders/"+name_fragment+".png"
     image_tag(image_name, :alt => "")
